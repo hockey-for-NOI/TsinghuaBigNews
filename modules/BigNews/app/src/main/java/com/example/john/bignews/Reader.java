032b569efc59a1c406a631a4292f598dc280e10a
cy <cy>
2017-09-13 16:39:29 +0800

    Unknown err

--- modules/BigNews/app/src/main/java/com/example/john/bignews/Reader.java
+++ modules/BigNews/app/src/main/java/com/example/john/bignews/Reader.java
@@ -5,22 +5,27 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.hq.usermanager.Newsdata;
 import com.example.sth.net.ImageLoader;
+import com.google.common.reflect.Parameter;
 
 import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+
+import static java.lang.Math.floor;
 
 public class Reader extends AppCompatActivity {
 
@@ -64,29 +69,41 @@ public class Reader extends AppCompatActivity {
 
     private void prepare()
     {
+        for (int i=0; i<readerLayout.getChildCount(); i++)
+            if (readerLayout.getChildAt(i) instanceof ImageView)
+                readerLayout.removeView(readerLayout.getChildAt(i));
         Newsdata tmp = Newsdata.get(bundle.getString("ID"));
         titleView.setText(tmp.getTitle());
         contentView.setText(tmp.getContent());
         ArrayList<String> list = tmp.getPictures();
-        for (String pic : list)
+        for (int pid=0; pid<list.size(); pid++)
         {
-            final ImageView imageView = new ImageView(this);
-            imageView.setMaxWidth(MainActivity.static_width);
-            imageView.setMaxHeight(MainActivity.static_height);
+            String pic = list.get(pid);
+            ImageView imageView = new ImageView(this);
             readerLayout.addView(imageView);
             imgURL = pic;
-            new Thread(new Runnable() {
+
+            new Thread() {
+                ImageView t;
+
+                public Thread set(ImageView t) {
+                    this.t = t;
+                    return this;
+                }
+
                 @Override
                 public void run() {
                     final Drawable drawable = ImageLoader.loadImageFromNetwork(imgURL);
-                    imageView.post(new Runnable() {
+                    t.post(new Runnable() {
                         @Override
                         public void run() {
-                            imageView.setImageDrawable(drawable);
+                            t.setImageDrawable(drawable);
+                            t.setLayoutParams(new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
                         }
                     });
                 }
-            }).start();
+            }.set(imageView).start();
         }
     }
 }
